81bace7d918987e499f1222bbd3c34b0677d86ad
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-06-22 16:51:33 +0600

    Connect in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,88 @@
+import { useState } from "react";
+
+import SearchBox from "./components/SearchBox";
+
+import API from "./services/api";
+
+
+
 function App() {
 
 
-  return (
-    <>
+    const [user,setUser] = useState(null);
 
-      <h1>
-        CPilot 🚀
-      </h1>
 
-    </>
-  );
+    const analyzeUser = async(handle)=>{
+
+
+        const response = await API.get(
+            `/user/${handle}`
+        );
+
+
+        setUser(response.data.data);
+
+
+    };
+
+
+
+    return (
+
+        <div>
+
+
+            <h1>
+                CPilot 🚀
+            </h1>
+
+
+            <SearchBox 
+                onSearch={analyzeUser}
+            />
+
+
+            {
+                user && (
+
+                    <div>
+
+
+                        <img 
+                        src={user.avatar}
+                        />
+
+
+                        <h2>
+                            {user.handle}
+                        </h2>
+
+
+                        <p>
+                            Rating: {user.rating}
+                        </p>
+
+
+                        <p>
+                            Rank: {user.rank}
+                        </p>
+
+
+                        <p>
+                            Max Rating: {user.maxRating}
+                        </p>
+
+
+                    </div>
+
+                )
+            }
+
+
+        </div>
+
+    );
+
 
 }
 
