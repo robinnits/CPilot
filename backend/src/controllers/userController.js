30193d8a1cf0025fd2dd67df300d56483aea3651
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-06-23 10:37:03 +0600

    Create Analytic Controller

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -39,8 +39,109 @@ const getUserProfile = async(req,res)=>{
 
 };
 
+const getUserAnalytics = async(req,res)=>{
+
+
+    try{
+
+
+        const handle = req.params.handle;
+
+
+        const submissions = 
+        await codeforcesService.getSubmissions(handle);
+
+
+
+        let accepted = 0;
+
+        let solvedProblems = new Set();
+
+
+
+        submissions.forEach((sub)=>{
+
+
+            if(sub.verdict === "OK"){
+
+
+                accepted++;
+
+
+                const problemID =
+
+                sub.problem.contestId 
+                + "-" +
+                sub.problem.index;
+
+
+
+                solvedProblems.add(problemID);
+
+
+            }
+
+
+        });
+
+
+
+        const total = submissions.length;
+
+
+        const accuracy = 
+        ((accepted/total)*100).toFixed(2);
+
+
+
+        res.json({
+
+            success:true,
+
+
+            data:{
+
+
+                totalSubmissions:total,
+
+                acceptedSubmissions:accepted,
+
+                solvedProblems:solvedProblems.size,
+
+                accuracy:accuracy
+
+
+            }
+
+
+        });
+
+
+
+    }
+
+
+    catch(error){
+
+
+        res.status(500).json({
+
+            success:false,
+
+            message:"Analytics failed"
+
+        });
+
+
+    }
+
+
+
+};
+
 
 
 module.exports = {
-    getUserProfile
+    getUserProfile,
+    getUserAnalytics
 };
