d45e902be2c023a9f61300c83b96d3015115df88
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-06-24 16:35:48 +0600

    add ratinghistory charts' code

---
+++ frontend/src/components/charts/RatingHistoryChart.jsx
@@ -0,0 +1,134 @@
+import {
+
+    LineChart,
+
+    Line,
+
+    XAxis,
+
+    YAxis,
+
+    Tooltip,
+
+    ResponsiveContainer,
+
+    CartesianGrid
+
+
+} from "recharts";
+
+
+
+function RatingHistoryChart({ratingHistory}){
+
+
+    if(
+
+        !ratingHistory ||
+
+        ratingHistory.length === 0
+
+    ){
+
+
+        return (
+
+            <h3>
+
+                No rated contests yet 🚀
+
+            </h3>
+
+        );
+
+
+    }
+
+
+
+    return (
+
+
+        <div>
+
+
+            <h3>
+
+                Rating Progress 📈
+
+            </h3>
+
+
+
+            <ResponsiveContainer
+
+                width="100%"
+
+                height={350}
+
+            >
+
+
+                <LineChart
+
+                    data={ratingHistory}
+
+                >
+
+
+
+                    <CartesianGrid
+
+                    strokeDasharray="3 3"
+
+                    />
+
+
+
+                    <XAxis
+
+                    dataKey="date"
+
+                    />
+
+
+
+                    <YAxis />
+
+
+
+                    <Tooltip />
+
+
+
+                    <Line
+
+                    type="monotone"
+
+                    dataKey="newRating"
+
+                    strokeWidth={3}
+
+                    />
+
+
+
+                </LineChart>
+
+
+
+            </ResponsiveContainer>
+
+
+
+        </div>
+
+
+    );
+
+
+}
+
+
+
+export default RatingHistoryChart;
