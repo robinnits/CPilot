fa64596f63d1ec475011e2210654f346f5f59ce8
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-06-24 17:29:26 +0600

    add rank progress card code

---
+++ frontend/src/components/RankProgressCard.jsx
@@ -0,0 +1,118 @@
+function RankProgressCard({rankProgress}){
+
+
+    return (
+
+        <div>
+
+
+            <h2>
+                🚀 Road To {rankProgress.targetRank}
+            </h2>
+
+
+
+            <h3>
+
+                {rankProgress.currentRating}
+
+                {" / "}
+
+                {rankProgress.targetRating}
+
+            </h3>
+
+
+
+            <p>
+
+                Progress:
+
+                {" "}
+
+                {rankProgress.progress}
+
+                %
+
+            </p>
+
+
+
+            <p>
+
+                Need +
+
+                {rankProgress.ratingNeeded}
+
+                rating
+
+            </p>
+
+
+
+
+
+            <h3>
+
+                Focus Areas 🧠
+
+            </h3>
+
+
+
+
+            {
+
+                rankProgress.focusAreas.map(skill=>(
+
+
+                    <div key={skill.skill}>
+
+
+                        <p>
+
+
+                            {skill.skill}
+
+
+                            {" : "}
+
+
+                            {skill.score}
+
+
+                            /100
+
+
+                        </p>
+
+
+
+                        <p>
+
+                            {skill.level}
+
+                        </p>
+
+
+
+                    </div>
+
+
+                ))
+
+            }
+
+
+
+
+        </div>
+
+    );
+
+
+}
+
+
+
+export default RankProgressCard;
