e59e0045d565b26db7a3b2a12dd66721a1d17c56
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-06-25 12:52:34 +0600

    searchbox updated

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
 
 
-
 function SearchBox({ onSearch }) {
 
 
-    const [handle, setHandle] = useState("");
+    const [handle,setHandle] =
+    useState("");
 
 
 
-    const handleSubmit = () => {
+    const submit = ()=>{
 
 
-        if(handle.trim() !== ""){
+        if(handle.trim()){
 
 
             onSearch(
-
                 handle.trim()
-
             );
 
 
@@ -32,74 +30,90 @@ function SearchBox({ onSearch }) {
 
     return (
 
-
-        <div>
-
-
-
-            <input
-
-
-                type="text"
+        <div
+        className="
+        flex
+        justify-center
+        my-10
+        "
+        >
 
 
-                placeholder="Enter Codeforces handle"
+            <div
+            className="
+            w-[500px]
+            flex
+            items-center
+            border-b
+            border-gray-600
+            "
+            >
 
+
+                <input
 
                 value={handle}
 
-
+                placeholder="Codeforces User Handle"
 
                 onChange={(e)=>
 
-                    setHandle(
-
-                        e.target.value
-
-                    )
+                    setHandle(e.target.value)
 
                 }
-
 
 
                 onKeyDown={(e)=>{
 
 
-                    if(e.key === "Enter"){
+                    if(e.key==="Enter")
 
-
-                        handleSubmit();
-
-
-                    }
+                        submit();
 
 
                 }}
 
 
-            />
+                className="
+                flex-1
+                bg-transparent
+                outline-none
+                text-center
+                text-2xl
+                py-3
+                text-white
+                placeholder-gray-500
+                "
+
+                />
 
 
 
 
+                <button
 
-            <button
+                onClick={submit}
 
-                onClick={handleSubmit}
+                className="
+                px-5
+                py-2
+                rounded-lg
+                bg-blue-600
+                hover:bg-blue-700
+                "
 
-            >
-
+                >
 
                 Analyze
 
+                </button>
 
-            </button>
 
 
+            </div>
 
 
         </div>
-
 
     );
 
