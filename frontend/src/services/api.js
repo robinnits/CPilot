737454ad0d665ea355dd17013b9397a1ab4f9540
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-07-24 14:14:22 +0530

    update api.js for the deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-
-const API = axios.create({
-
-    baseURL:"http://localhost:8000/api"
-
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-
-export default API;
+export default api;
