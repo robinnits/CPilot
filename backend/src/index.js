e2daeb884f35053182d96fdcb95ce3e4fd962340
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-07-24 13:48:15 +0530

    port added for the deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
