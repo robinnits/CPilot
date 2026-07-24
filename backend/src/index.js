14694c21e9347ab71fcc4ea3d84c2184b4a4cc00
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-07-24 13:55:34 +0530

    Update the CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,12 @@ app.use(express.json());
 
 const userRoutes = require("./routes/userRoutes");
 
-app.use("/api/user", userRoutes);
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
 
 
 app.get("/", (req, res) => {
